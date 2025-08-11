6e965849fa551ae17582431bc4dbb83cb17d1c24
Sergio Rojas <srojas@usb.ve>
2025-08-11 10:56:48 -0400

    Add files via upload
    my_linearFit.py update

--- PlottingProgram/my_linearFit.py
+++ PlottingProgram/my_linearFit.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Sequence, Union
@@ -55,7 +56,7 @@ def plot_data(
         slope * x_arr + intercept,
         color='C2',
         linewidth=line_width,
-        label= f'Linear Fit: ${ylabel.split(' ')[0]} = {slope:.3f}\\,{xlabel.split(' ')[0]}$' + f'${intercept:.3f}$'
+        label= f'Linear Fit: ${ylabel.split(' ')[0]} = {slope:.3f}\\,{xlabel.split(' ')[0]}$' + f'${intercept:+.3f}$'
     )
 
     # Labels, title, ticks
@@ -63,7 +64,7 @@ def plot_data(
     ax.set_ylabel(ylabel, fontsize=label_font)
     ax.tick_params(labelsize=tick_font)
     ax.set_title(
-        f'Linear Fit: ${ylabel.split(' ')[0]} = {slope:.3f}\\,{xlabel.split(' ')[0]}$' + f'${intercept:.3f}$',
+        f'Linear Fit: ${ylabel.split(' ')[0]} = {slope:.3f}\\,{xlabel.split(' ')[0]}$' + f'${intercept:+.3f}$',
         fontsize=label_font
     )
 
@@ -73,20 +74,36 @@ def plot_data(
     plt.show()
 
     # Send the fit back to the calling instruction
-    thefit = f'Linear Fit:\n\t   {ylabel.split(' ')[0]} = {slope:.3f}·{xlabel.split(' ')[0]}' + f'{intercept:.3f}'
+    thefit = f'Linear Fit:\n\t   {ylabel.split(' ')[0]} = {slope:.3f}{xlabel.split(' ')[0]}' + f'{intercept:+.3f}'
     return thefit    # end function
 
-# Example usage with Python lists
+# ENTERING THE DATA. Any example could be deleted when using your own data
+# Example 1
+# horizontal axis data
 t = [0.415, 0.463, 0.495, 0.543, 0.59, 0.623, 0.67, 0.703, 0.75,
               0.798, 0.83, 0.878, 0.911]
-
+# vertical axis data
 y = [94.502235, 117.412667, 135.238955, 162.386387, 190.20431,
               213.717107, 245.60903, 273.357827, 308.50975, 347.228182,
               381.35147, 422.513902, 456.130699]
-
 xlabel='t (s)'
 ylabel='y (m)'
 thefit = plot_data(t, y, xlabel, ylabel)
 print(thefit)
+#----------------End example 1
+
+# Example 2
+xlabel='T (K)'
+ylabel='P (atm)'
+
+# horizontal axis data
+T = [ 173, 223, 273, 323, 373, 423]
+
+# vertical axis data
+P = [ 36.0, 46.4, 56.7, 67.1, 77.5, 88.0]
+
+thefit = plot_data(T, P, xlabel, ylabel)
+print(thefit)
+#----------------End example 2
 
 
